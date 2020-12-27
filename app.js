c360d24e88caaeda203e72655eee878b741e3393
i'mfebri <73212374+febriadj@users.noreply.github.com>
2020-12-27 15:12:55 +0700

    konfigurasi disk storage multer

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ const path = require('path');
 const multer = require('multer');
 const app = express();
 
-const upload = multer({ dest: './public/uploads' });
+const storage = multer.diskStorage({
+  destination: (err, file, cb) => {
+    cb(null, path.join(__dirname, '/public/uploads'));
+  },
+  filename: (err, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // set view engine ejs
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join('/public/uploads')));
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -20,6 +31,13 @@ app.get('/', (req, res) => {
 
 app.post('/', upload.single('upload_file'), (req, res) => {
   console.log(req.file, req.body);
+  res.end();
+});
+
+app.get('/image', (req, res) => {
+  res.render('image', {
+    title: 'Multer Upload - Images'
+  })
 });
 
 const port = process.env.PORT || 3000;
